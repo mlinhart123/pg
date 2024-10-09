25e6166ef38a4678f74958a5fb7f81c21442e104
mlinhart123 <mlinhart7@gmail.com>
2024-10-09 19:28:25 +0200

    Update first.py

--- first.py
+++ first.py
@@ -1,10 +1,2 @@
-# Program pro zjištění, zda je číslo sudé nebo liché
-
-# Zadej číslo
-cislo = int(input("Zadej číslo: "))
-
-# Kontrola, zda je číslo sudé nebo liché
-if cislo % 2 == 0:
-    print(f"{cislo} je sudé číslo.")
-else:
-    print(f"{cislo} je liché číslo.")
+# sudy_nebo_lichy
+number = int(input("zadejte
