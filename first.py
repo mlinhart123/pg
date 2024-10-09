c67fe27310a3a9a3978c3b723b3cbaeceae02726
mlinhart123 <mlinhart7@gmail.com>
2024-10-09 19:46:40 +0200

    Update first.py

--- first.py
+++ first.py
@@ -1,2 +1,7 @@
 # sudy_nebo_lichy
-number = int(input("zadejte
+number = int(input("zadejte cislo: "))
+if (number % 2) == 0:
+  print("{0} cislo je sudé".format(number))
+else:
+  print("{0} císlo je liché".format(number))
+  
